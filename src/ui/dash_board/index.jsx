75ad88c285dbce6b26822b9e4074e4aa00fa71bc
Sarpong97-Tom <sarpongthomas05@gmail.com>
2021-05-24 03:22:31 +0000

    Added statistics page

--- src/ui/dash_board/index.jsx
+++ src/ui/dash_board/index.jsx
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookReader, faChartLine, faDesktop, faPray, faRunning, faUser } from '@fortawesome/free-solid-svg-icons'
 import "./styles/index.scss"
 import { UserOutlined } from "@ant-design/icons"
+import StatisticsPage from '../statistics_page'
 const LandingPage = () => {
     const [state,setState] = useState({
         menuCollapse:false
@@ -25,14 +26,14 @@ const LandingPage = () => {
                 <Divider/>
                 <Menu theme = "light" style = {{padding:"2em"}} mode = "inline" defaultOpenKeys = {["1"]} >
                     
-                        <Link>
+                        <Link to = "/" >
                         <Menu.Item style = {{fontSize:"1.1rem"}}  icon = {<FontAwesomeIcon style = {{marginRight:"1em"}} icon = {faChartLine} />} key = "1" >
                             <span hidden = {state.menuCollapse} >Statistics</span>
                         </Menu.Item>
                         
                         </Link>
                         <Divider/>
-                        <Link>
+                        <Link to = "/sam-tv" >
                         <Menu.Item style = {{fontSize:"1.1rem"}}  icon = {<FontAwesomeIcon style = {{marginRight:"1em"}} icon = {faDesktop} />} key = "2" >
                            
                             <span hidden = {state.menuCollapse} > Sam tv</span>
@@ -41,24 +42,22 @@ const LandingPage = () => {
                         </Link>
                        
                         <Divider/>
-                        <Link>
+                        <Link to = "motivation" >
                         <Menu.Item style = {{fontSize:"1.1rem"}}  icon = {<FontAwesomeIcon style = {{marginRight:"1em"}} icon = {faRunning} />} key = "3" >
                         <span hidden = {state.menuCollapse} > Motovations</span>
-
-                            
                         </Menu.Item>
                         </Link>
                         <Divider/>
-                        <Link>
+                        <Link to = "prayer-requests" >
                         <Menu.Item style = {{fontSize:"1.1rem"}}  icon = {<FontAwesomeIcon style = {{marginRight:"1em"}} icon = {faPray} />} key = "3" >
-                        <span hidden = {state.menuCollapse} > Prayers</span>
+                        <span hidden = {state.menuCollapse} > Prayers Req... </span>
 
                             
                         </Menu.Item>
                         </Link>
 
                         <Divider/>
-                        <Link>
+                        <Link to = "sermons" >
                         <Menu.Item style = {{fontSize:"1.1rem"}}  icon = {<FontAwesomeIcon style = {{marginRight:"1em"}} icon = {faBookReader} />} key = "3" >
                         <span hidden = {state.menuCollapse} > Sermones</span>
 
@@ -66,7 +65,7 @@ const LandingPage = () => {
                         </Menu.Item>
                         </Link>
                         <Divider/>
-                        <Link>
+                        <Link to = "users" >
                         <Menu.Item style = {{fontSize:"1.1rem"}}  icon = {<FontAwesomeIcon style = {{marginRight:"1em"}} icon = {faUser} />} key = "3" >
                         <span hidden = {state.menuCollapse} > Users</span>
 
@@ -89,7 +88,7 @@ const LandingPage = () => {
                 </Layout.Header>
                 <Layout.Content>
                     <Switch>
-                        <Route path = ""  />
+                        <Route path = "/" component = {StatisticsPage}  />
 
                     </Switch>
                 </Layout.Content>
---
+++ src/ui/statistics_page/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StatisticsPage = () => {
+    return (
+        <div>
+            <h1>Statistics Page</h1>
+        </div>
+    )
+}
+
+export default StatisticsPage
