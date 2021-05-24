9d8f2bf2aadc5e0fddd9c524e44fc708c79bf453
Sarpong97-Tom <sarpongthomas05@gmail.com>
2021-05-24 10:26:24 +0000

    added firebase config

---
+++ src/utils/networks/firebaseConfig.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/firestore/'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDYpBkB0nj2QAXdkvQQ5lXytn1XOBtk_3U",
+    authDomain: "sam-tv-ebd2b.firebaseapp.com",
+    projectId: "sam-tv-ebd2b",
+    storageBucket: "sam-tv-ebd2b.appspot.com",
+    messagingSenderId: "300037909449",
+    appId: "1:300037909449:web:91e9afd2d7330471911618",
+    measurementId: "G-L38Q39G37D"
+  };
+
+
+firebase.initializeApp(firebaseConfig)
+export default firebase
