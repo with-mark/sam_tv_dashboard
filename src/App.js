1d381cc9f4c2e27c20c9c0e04262af5bc946dfc1
Sarpong97-Tom <sarpongthomas05@gmail.com>
2021-05-24 01:41:11 +0000

    added all

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'antd/dist/antd.css';
 import {BrowserRouter as Router,Switch,Route } from 'react-router-dom'
 import LoginPage from './ui/login_page';
 import Error404Page from './ui/error/404Page';
--- src/ui/login_page/index.jsx
+++ src/ui/login_page/index.jsx
@@ -1,11 +1,55 @@
-import { Card } from 'antd'
+import { Button, Card, Form, Image, Input } from 'antd'
 import React from 'react'
 import './styles/index.scss'
+import logo from '../../assets/images/logo.png'
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
 
 const LoginPage = () => {
     return (
         <div className = "login-page" >
-            <Card id ="login-card"></Card>
+            <Card id ="login-card">
+                <div className="logo">
+                <Image id ="logo"  preview = {false} src = {logo} alt = "Logo" />
+
+                </div>
+                <div className="header">
+                    <h4 className="text-center ml-5 mb-3">
+                        Adminstrator's Login
+                    </h4>
+                </div>
+            <Form
+      {...layout}
+      name="loginForm"
+     
+    >
+      <Form.Item
+      className = "form-item"
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+      className = "form-item"
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+  
+
+        <Button id = "submit-button" shape = "round" type="primary" htmlType="submit">
+          Submit
+        </Button>
+    </Form>
+            </Card>
         </div>
     )
 }
