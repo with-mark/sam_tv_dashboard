5746f5a79c3f8265c87a970f87f3939bf5cbd51f
Thomas <sarpongthomas05@gmail.com>
2021-06-14 17:01:13 -0400

    added motivation crud operations

--- src/state_mamger/functions/motivations.js
+++ src/state_mamger/functions/motivations.js
@@ -1,7 +1,20 @@
+import { message } from "antd"
 import { db } from "../../utils/networks/firebaseConfig"
+import pushNotification from "../../utils/pushNotification"
 const FETCH_MOTIVATION_REQUEST = "FETCH_MOTIVATION_REQUEST"
 const FETCH_MOTIVATION_SUCCESS = "FETCH_MOTIVATION_SUCCESS"
-const FETCH_MOTIVATION_FAILURE = "FETCH_MOTIVATION_FAILURE"
+// const FETCH_MOTIVATION_FAILURE = "FETCH_MOTIVATION_FAILURE"
+const POST_MOTIVATION_REQUEST = "POST_MOTIVATION_REQUEST"
+const POST_MOTIVATION_COMPLETED = "POST_MOTIVATION_COMPLETED"
+
+const DELETE_MOTIVATION_REQUEST = "DELETE_MOTIVATION_REQUEST"
+const DELETE_MOTIVATION_COMPLETED = "DELETE_MOTIVATION_COMPLETED"
+
+const UPDATE_MOTIVATION_REQUEST = "UPDATE_MOTIVATION_REQUEST"
+const UPDATE_MOTIVATION_COMPLETED = "UPDATE_MOTIVATION_COMPLETED"
+
+
+const collectionName = "motivation"
 
 const fetchMotivationRequest= ()=>{
     return{
@@ -16,11 +29,87 @@ const fetchMotivationSuccess= motivations=>{
 }
 
 
+const postMotivationRequest = ()=>{
+    return{
+        type: POST_MOTIVATION_REQUEST
+    }
+}
+
+const postMotivationCompleted=()=>{
+    return{
+        type:POST_MOTIVATION_COMPLETED
+    }
+}
+
+
+const deleteMotivationRequest = ()=>{
+    return{
+        type: DELETE_MOTIVATION_REQUEST
+    }
+}
+
+const deleteMotivationCompleted=()=>{
+    return{
+        type:DELETE_MOTIVATION_COMPLETED
+    }
+}
+
+const updateMotivationRequest = ()=>{
+    return{
+        type: UPDATE_MOTIVATION_REQUEST
+    }
+}
+
+const updateMotivationCompleted=()=>{
+    return{
+        type:UPDATE_MOTIVATION_COMPLETED
+    }
+}
+
+
+export const updateMotivation =(motivation)=>dispatch=>{
+    dispatch(updateMotivationRequest())
+    db.collection(collectionName).doc(motivation.id).update({...motivation})
+    .then(()=>{
+        dispatch(updateMotivationCompleted())
+        message.success("Motivation updated successfully")
+    }).catch(err=>{
+        dispatch(updateMotivationCompleted())
+        message.error(`Updating motivation failed, reason: ${String(err)}`)
+    })
+}
+
+
+export const deleteMotivation=(motivation)=>dispatch=>{
+    dispatch(deleteMotivationRequest())
+    db.collection(collectionName).doc(motivation.id).delete()
+    .then(()=>{
+        dispatch(deleteMotivationCompleted())
+        message.success("Motivation deleted successfully")
+    }).catch(err=>{
+        dispatch(deleteMotivationCompleted())
+        message.error(`Deleting motivation failed, reason: ${String(err)}`)
+    })
+}
+
+
+
+export const postMotivation=(motivation)=>dispatch=>{
+    dispatch(postMotivationRequest())
+    db.collection(collectionName).add(motivation).then(()=>{
+        dispatch(postMotivationCompleted())
+        pushNotification("Motivstion!",motivation.title,"sam_tv_motivation")
+        message.success("Motivation posted successfully")
+    }).catch(err=>{
+        dispatch(postMotivationCompleted())
+        message.error(`Posting motivation failed, reason: ${String(err)}`)
+    })
+}
 
 
 export const fetchMotivations =()=>dispatch=>{
    dispatch(fetchMotivationRequest())
-    const ref = db.collection("motivation")
+    const ref = db.collection(collectionName)
     let motivations = []
     ref.onSnapshot((querySnapshot)=>{
         querySnapshot.forEach(doc=>{
@@ -39,8 +128,13 @@ export const fetchMotivations =()=>dispatch=>{
 
 
 
+
+
 const initialState = {
 loading:false,
+postLoading:false,
+deleteLoading:false,
+updateLoading:false,
 data:[]
 }
 
@@ -57,11 +151,38 @@ export const motivationReducer = (state = initialState, { type, payload }) => {
             loading:false,
             data:payload
         }
-    case FETCH_MOTIVATION_FAILURE:
+
+    case POST_MOTIVATION_REQUEST:
         return{
             ...state,
-            loading:false
+            postLoading:true
         }
+    case POST_MOTIVATION_COMPLETED:
+        return{
+            ...state,
+            postLoading:false
+        }
+    case UPDATE_MOTIVATION_REQUEST:
+        return{
+            ...state,
+            updateLoading:true
+        }
+    case UPDATE_MOTIVATION_COMPLETED:
+        return{
+            ...state,
+            updateLoading:false
+        }
+    case DELETE_MOTIVATION_REQUEST:
+        return{
+            ...state,
+            deleteLoading:true
+        }
+    case DELETE_MOTIVATION_COMPLETED:
+        return{
+            ...state,
+            deleteLoading:false
+        }
+
     default:
         return state
     }
