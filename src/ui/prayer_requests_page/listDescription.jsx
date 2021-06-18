dc23b52dc701890ebfe9bfc8179a68a400e73111
Thomas <sarpongthomas05@gmail.com>
2021-06-18 03:52:48 +0000

    added all

--- src/ui/prayer_requests_page/listDescription.jsx
+++ src/ui/prayer_requests_page/listDescription.jsx
@@ -1,7 +1,7 @@
 import { Tag } from 'antd'
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
-import { getDateInWords } from '../../utils/dateTimeConvert'
+// import { getDateInWords } from '../../utils/dateTimeConvert'
 
 const ListDescription = ({item}) => {
     return (
@@ -9,7 +9,7 @@ const ListDescription = ({item}) => {
            
             <Row>
                 <Col xs = "6" sm = "6" lg = "3" > <small> <b>Author: </b> {item.author}</small></Col>
-                <Col xs = "6" sm = "6" lg = "3" > <small> <b>Posted at: </b> {getDateInWords(item.date.toDate())} </small> </Col>
+                {/* <Col xs = "6" sm = "6" lg = "3" > <small> <b>Posted at: </b> {getDateInWords(item.date.toDate())} </small> </Col> */}
             </Row>
             <p> {item.description}  </p>
                 {item.is_read? <Tag color  = "green" >Read</Tag>:<Tag color  = "red" >Not read</Tag>} 
